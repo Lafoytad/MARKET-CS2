930ea43791b851520f051bb7b1f3b374655aa0ab
Matthew <wispmatvei@gmail.com>
2024-06-12 23:54:42 +0300

    home page layout

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,48 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+"use client"; // 1
+
+import React, { useState } from "react"; // 1
+import "./globals.css";
+import styles from "./page.module.scss";
+import Image from "next/image"; // 1
+import home from "../../public/icons/home_icon_png.png"; // 1
+import homeHover from "../../public/icons/home_icon_pngHover.png"; // 1
+import { useRouter } from "next/navigation"; // 1
 
 export default function Home() {
+  const [isHovered, setIsHovered] = useState(false); // 1
+  const router = useRouter(); // 1
+
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+      <header className={styles.header}>
+        <ul>
+          <li>Инвентарь</li>
+          <li>Магазин</li>
+          <li>Новости</li>
+        </ul>
+      </header>
+
+      <div className={styles.icons}>
+        <ul>
+          <li
+            onMouseOver={() => setIsHovered(true)}
+            onMouseOut={() => setIsHovered(false)}
+            onClick={() => router.push("/")}
           >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+            <Image src={isHovered ? homeHover : home} alt="Home" />
+          </li>
+          <li>
+            <Image src={home} alt="Home"></Image>
+          </li>
+        </ul>
       </div>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+      <aside className={styles.aside}></aside>
 
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      <section className={styles.section}>
+        <aside className={styles.one}>dxvdx</aside>
+        <aside className={styles.two}>ddd</aside>
+      </section>
     </main>
   );
 }
