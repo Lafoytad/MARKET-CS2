70aef0489bad9c61c667a948c0d5e74681d654d5
Matthew <wispmatvei@gmail.com>
2024-07-09 18:20:32 +0300

    Deploy2

--- src/app/(main)/store/copenhagen/page.js
+++ src/app/(main)/store/copenhagen/page.js
@@ -1,6 +1,6 @@
 import Header from "@/app/components/organisms/header/Header";
 import Sidebar from "@/app/components/organisms/sidebar/Sidebar";
-import StoreCopenhagen from "@/app/components/organisms/store/copenhagen/copenhagen";
+import StoreCopenhagen from "@/app/components/organisms/store/copenhagen/Copenhagen";
 
 export default function Home() {
   return (
