cad4640824f7996ccf8e56895adc593babf2f32b
Matthew <wispmatvei@gmail.com>
2024-07-12 14:53:58 +0300

    Deploy10

--- next.config.mjs
+++ next.config.mjs
@@ -10,7 +10,7 @@ const nextConfig = {
     return config;
   },
   env: {
-    API_URL: process.env.API_URL,
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
     API_TOKEN: process.env.API_TOKEN,
   },
 };
